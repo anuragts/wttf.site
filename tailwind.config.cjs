7635eb4435b8d952c048dafac1b8bfd122faa1a2
Anurag30112003 <anuragsharma011011@gmail.com>
2022-10-23 00:09:46 +0530

    made home page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      screens: {
+        fd: "300px",
+
+        sm: "450px",
+
+        md: "768px",
+
+        lg: "1350px",
+      },
+    },
+  },
+  plugins: [],
+};
